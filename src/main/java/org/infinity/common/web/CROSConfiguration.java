a6097c0f89f1f46713ae480118dc7c432f9b2e90
Ricky <w_ziwei2004@163.com>
2024-12-18 22:51:15 +0800

    解决跨域问题

---
+++ src/main/java/org/infinity/common/web/CROSConfiguration.java
@@ -0,0 +1,27 @@
+package org.infinity.common.web;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+/**
+ * @author Ricky
+ * @version 1.0
+ * @date 2024/12/18
+ * @className CROSConfiguration
+ * @desc
+ */
+@Configuration
+public class CROSConfiguration {
+
+    public static final String HOST = "localhost";
+    public static final String PORT = "5173";
+
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://" + HOST + ":" + PORT)
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+
+}
